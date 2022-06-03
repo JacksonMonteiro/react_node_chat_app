e2bb29f70f962ba3a4dd20b196867c778a3d1104
jacksonmonteiro <infor.jackson324@gmail.com>
2022-06-03 15:55:15 -0300

    get all contacts feature

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,16 @@
-const { register, login, setAvatar, getAllUsers } = require('../controllers/userController');
-const router  = require('express').Router();
+const {
+    register,
+    login,
+    setAvatar,
+    getAllUsers
+} = require('../controllers/userController');
 
-router.get('/allusers:id', getAllUsers)
+const router = require('express').Router();
+
 
 router.post('/register', register);
 router.post('/login', login);
 router.post('/setAvatar/:id', setAvatar);
+router.get('/allusers/:id', getAllUsers)
 
 module.exports = router;
