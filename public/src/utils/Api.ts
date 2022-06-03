79d8ceb168b366340d17c8abfa74449b01e65abf
jacksonmonteiro <infor.jackson324@gmail.com>
2022-06-03 13:41:26 -0300

    chat_container

--- public/src/utils/Api.ts
+++ public/src/utils/Api.ts
@@ -3,3 +3,4 @@ const baseUrl = "http://localhost:5000";
 export const registerRoute = `${baseUrl}/api/auth/register`;
 export const loginRoute = `${baseUrl}/api/auth/login`;
 export const setAvatarRoute = `${baseUrl}/api/auth/setAvatar`;
+export const allUsersRoute = `${baseUrl}/api/auth/allusers`;
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,7 @@
-const { register, login, setAvatar } = require('../controllers/userController');
+const { register, login, setAvatar, getAllUsers } = require('../controllers/userController');
 const router  = require('express').Router();
+
+router.get('/allusers:id', getAllUsers)
 
 router.post('/register', register);
 router.post('/login', login);
