c5876c195a303f6539e110a985fbe73cf425d00e
jacksonmonteiro <infor.jackson324@gmail.com>
2022-06-06 14:30:37 -0300

    socket integration back-end

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const app = express();
 const userRoutes = require('./routes/userRoutes');
 const messageRoutes = require('./routes/messagesRoute');
+const socket = require('socket.io');
 require('dotenv').config();
 
 app.use(cors());
@@ -26,3 +27,26 @@ mongoose.connect(process.env.MONGO_URL, {
 const server = app.listen(process.env.PORT, () => {
     console.log(`Servidor iniciado na porta: ${process.env.PORT}`);
 }); 
+
+const io = socket(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        credentials: true
+    },
+});
+
+global.onlineUsers = new Map();
+
+io.on('connection', (socket) => {
+    global.chatSocket = socket;
+    socket.on('add-user', (userId) => {
+        onlineUsers.set(userId, socket.id);
+    });
+
+    socket.on('send-msg', (data) => {
+        const sendUserSocket = onlineUsers.get(data.to); 
+        if (sendUserSocket) {
+            socket.to(sendUserSocket).emit('msg-receive', data.message);
+        }
+    });
+});
